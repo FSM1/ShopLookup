312cc77ec9182a0c55e31325916c5fc31dbb1d57
Michael Yankelev <myankelev@gmail.com>
2019-09-18 02:38:03 +0200

    clear out styles

--- app/components/AppWrapper/index.tsx
+++ app/components/AppWrapper/index.tsx
@@ -18,13 +18,7 @@ const styles = theme => createStyles({
     textAlign: 'center',
     color: 'white',
   },
-  toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
+
   content: {
     flexGrow: 1,
     minHeight: '100vh',
